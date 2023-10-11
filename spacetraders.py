824650719d7806e423154c99f1943d52ee5d85e4
francis <francisg@esdallas.org>
2023-10-11 14:50:27 -0500

    get ship location and list, and get locationDetail

--- spacetraders.py
+++ spacetraders.py
@@ -1,9 +1,10 @@
 import requests
 import re
 access_token = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZGVudGlmaWVyIjoiMjEzMTIzMjEzIiwidmVyc2lvbiI6InYyIiwicmVzZXRfZGF0ZSI6IjIwMjMtMDktMzAiLCJpYXQiOjE2OTY1NDY5MDUsInN1YiI6ImFnZW50LXRva2VuIn0.TnoSysC3TtT5iDXtMMCtwEywn585w4A1eWa95LRfpgsxu15_1gKouLdkw6EyXvYY7av_znwC6Tk4X-zrVCV5xMoVK5auAeJjHDman-ugZLo0my9DAOIl4Dq4oSUKYp1xx9V4vM_d6OHKNLWZJ65fbvrNLdLvA_F7OMCu2B9TxA_5MeYNcEOJ0vMZlNcvGENEUyjHeQ2l1WFa_EPorTXAVSwffQmvgo_X9cNC7F_7qo-JlWkIOIEcqAdVv6C5Cgpq5fJHxss0Nk0vVQXJATOinFyV4ndxEvDzLvsAkKwWCPzIFAGyhflxcezaI_16tDDTWEB1qXKUNgOIUBTNLo26Sw'
-
+#I realized publishing this doesnt really matter, its just an account for testing purposes  
 class setup:
     def __init__(self,name,faction,key = ''):
+        self.name = name
         if(key == ''):
             self.key = setup.createAccount(name,faction)
             if(self.key !=0):
@@ -30,11 +31,22 @@ class setup:
         return("Your access key is: " + match[0])
     def viewAgent(self):
         r = requests.get('https://api.spacetraders.io/v2/my/agent',headers ={ 'Authorization':'Bearer {}'.format(self.key)})
-        print(r.text)
+        return r.json()
+class ship(setup):
+    def shipList(self):
+        r = requests.get('https://api.spacetraders.io/v2/my/ships', headers ={ 'Authorization':'Bearer {}'.format(self.key)})
+        return r.json()
+    def shipLoc(shipNum,sList):
+        lst = sList
+        return [lst['data'][shipNum]['nav']['systemSymbol'],lst['data'][shipNum]['nav']['waypointSymbol']]
+    def locDetails(loc):
+        r = requests.get('https://api.spacetraders.io/v2/systems/' + str(loc[0]) +'/waypoints/'+str(loc[1]))
+        return r.json()
 
+    
 # r = requests.post(url=link, data={"symbol": "213123213",
 #     "faction": "COSMIC"})
- 
+
 # # extracting response text
 # output = r.text
 # r = requests.get('https://api.spacetraders.io/v2/my/agent',headers ={ 'Authorization':'Bearer {}'.format(access_token)})
