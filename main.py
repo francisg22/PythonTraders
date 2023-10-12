83d51a30a0ed74cd9299133e61533701069a5579
francis <francisg@esdallas.org>
2023-10-12 14:47:58 -0500

    systems section and json pretty print

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import spacetraders
-import json
+
 acc = spacetraders.ship('213123213','COSMIC','eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZGVudGlmaWVyIjoiMjEzMTIzMjEzIiwidmVyc2lvbiI6InYyIiwicmVzZXRfZGF0ZSI6IjIwMjMtMDktMzAiLCJpYXQiOjE2OTY1NDY5MDUsInN1YiI6ImFnZW50LXRva2VuIn0.TnoSysC3TtT5iDXtMMCtwEywn585w4A1eWa95LRfpgsxu15_1gKouLdkw6EyXvYY7av_znwC6Tk4X-zrVCV5xMoVK5auAeJjHDman-ugZLo0my9DAOIl4Dq4oSUKYp1xx9V4vM_d6OHKNLWZJ65fbvrNLdLvA_F7OMCu2B9TxA_5MeYNcEOJ0vMZlNcvGENEUyjHeQ2l1WFa_EPorTXAVSwffQmvgo_X9cNC7F_7qo-JlWkIOIEcqAdVv6C5Cgpq5fJHxss0Nk0vVQXJATOinFyV4ndxEvDzLvsAkKwWCPzIFAGyhflxcezaI_16tDDTWEB1qXKUNgOIUBTNLo26Sw')
 
 print(acc.viewAgent())
 shipList = acc.shipList()
-with open('shipList.json', 'w') as f:
-    json.dump(shipList, f)
+spacetraders.toFile(shipList,'shipList')
 print(shipList['data'][0]['nav']['waypointSymbol'])
 
 loc = spacetraders.ship.shipLoc(0,shipList)
-print(loc)
-print(spacetraders.ship.locDetails(loc))
+# print(loc)
+#print(spacetraders.systems.locDetails(loc))
+sys = spacetraders.systems.sysDetails('X1-PB4mkhgkvuytf6o7y8uop3')
+# print(spacetraders.systems.marketDetails(loc))
 # print(type(shipList))
 
+details = spacetraders.systems.marketDetails(loc)
+print(len(details['data']))
+print(sys)
+print(spacetraders.jsonToString(sys))
 # print(shipList['data'][0]['symbol'])
 # print(shipList['data'][1]['symbol'])
 # key = spacetraders.setup.createAccount('sduifhsduf2','COSMIC')
--- spacetraders.py
+++ spacetraders.py
@@ -1,7 +1,25 @@
 import requests
 import re
+import json
 access_token = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZGVudGlmaWVyIjoiMjEzMTIzMjEzIiwidmVyc2lvbiI6InYyIiwicmVzZXRfZGF0ZSI6IjIwMjMtMDktMzAiLCJpYXQiOjE2OTY1NDY5MDUsInN1YiI6ImFnZW50LXRva2VuIn0.TnoSysC3TtT5iDXtMMCtwEywn585w4A1eWa95LRfpgsxu15_1gKouLdkw6EyXvYY7av_znwC6Tk4X-zrVCV5xMoVK5auAeJjHDman-ugZLo0my9DAOIl4Dq4oSUKYp1xx9V4vM_d6OHKNLWZJ65fbvrNLdLvA_F7OMCu2B9TxA_5MeYNcEOJ0vMZlNcvGENEUyjHeQ2l1WFa_EPorTXAVSwffQmvgo_X9cNC7F_7qo-JlWkIOIEcqAdVv6C5Cgpq5fJHxss0Nk0vVQXJATOinFyV4ndxEvDzLvsAkKwWCPzIFAGyhflxcezaI_16tDDTWEB1qXKUNgOIUBTNLo26Sw'
 #I realized publishing this doesnt really matter, its just an account for testing purposes  
+#https://spacetraders.stoplight.io/docs/spacetraders/403855e2e99ad-purchase-ship
+def toFile(text, filename):
+    with open(filename + '.json', 'w') as f:
+        json.dump(text, f)
+def jsonToString(details):
+    str = json.dumps(details, indent=4, separators=(',', ': '), sort_keys=True)
+    return str
+    # if type == 'market':
+    #     for el in details['data'].keys():
+    #         if el == 'symbol':
+    #             continue
+    #         for i in details['data'][el]:
+    #             print(i)
+    #             print('\n')
+    # if type == 'sys':
+        
+
 class setup:
     def __init__(self,name,faction,key = ''):
         self.name = name
@@ -39,11 +57,33 @@ class ship(setup):
     def shipLoc(shipNum,sList):
         lst = sList
         return [lst['data'][shipNum]['nav']['systemSymbol'],lst['data'][shipNum]['nav']['waypointSymbol']]
+    
+class systems:
+    #if readme is used - remember to put loc is [system,waypoint]
     def locDetails(loc):
         r = requests.get('https://api.spacetraders.io/v2/systems/' + str(loc[0]) +'/waypoints/'+str(loc[1]))
         return r.json()
-
+    def systemList():
+        r = requests.get('https://api.spacetraders.io/v2/systems')
+        return r.json()
+    def sysDetails(system):
+        r = requests.get('https://api.spacetraders.io/v2/systems/' + system)
+        return r.json()
+    def sysWaypoints(system):
+        r = requests.get('https://api.spacetraders.io/v2/systems/' + system + '/waypoints')
+        return r.json()
+    def marketDetails(loc):
+        r = requests.get('https://api.spacetraders.io/v2/systems/' + str(loc[0]) +'/waypoints/'+ str(loc[1]) + '/market') 
+        return r.json()
+    def shipyardDetails(loc):
+        r = requests.get('https://api.spacetraders.io/v2/systems/' + str(loc[0]) +'/waypoints/'+ str(loc[1]) + '/shipyard')
+        return r.json()
+    def jumpGateDetails(loc):
+        r = requests.get('https://api.spacetraders.io/v2/systems/' + str(loc[0]) +'/waypoints/'+ str(loc[1]) + '/jump-gate')
+        return r.json()
     
+    
+               
 # r = requests.post(url=link, data={"symbol": "213123213",
 #     "faction": "COSMIC"})
 
